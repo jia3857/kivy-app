de749983523149894c76f2d6bac3785b9e70651c
mc <mc@mc.mc>
2020-07-25 15:12:16 -0700

    Added 12 sound effect

--- main.py
+++ main.py
@@ -10,32 +10,32 @@ from functools import partial
 
 class SoundBoard(App):
     def build(self):
-        #pasta aonde o programa vai procurar as musicas/sons para adiconionar na board
-        #no caso procura na pasta /music dentro da pasta atual.
+        self.musicFolder = os.path.dirname(os.path.abspath(__file__))+"/music"
 
-        '''gambi, avaliar aonde esta o arquivo ao inves de procurar o [3] dos files'''
-        self.musicFolder = os.listdir(os.path.dirname(os.path.abspath(__file__)))[3]+"/music"
-        
-        
-        self.files = os.listdir(os.path.dirname(os.path.abspath(self.musicFolder)))
+
+        self.files = os.listdir(self.musicFolder)
         #cria o layout para o programa
         self.masterlayout = BoxLayout(orientation="vertical")
         self.layout = GridLayout(cols=3)
         self.masterlayout.add_widget(self.layout)
         self.playing = []
         Window.borderless = False
-        Window.size = (500,500)
+        # Window.size = (500,500)
+        Window.size = (400, 720)
         contador = 0
-        for som in self.files:
+        for sound in self.files:
             #para cada som na pasta das musicas cria um botao novo
-            name, ext = os.path.splitext(som)
+            name, ext = os.path.splitext(sound)
+            if name == ".DS_Store":
+                contador-=1
+                continue
             btn = Button(text=name)
             #adiciona a funcao de tocar a musica ao apertar o botao
             btn.bind(on_press=partial(self.playSound, self.files[contador]))
             self.layout.add_widget(btn)
             contador+=1
         #botao para parar o player
-        btnStahp = Button(text="Staaahp", size_hint_x=1, size_hint_y = 0.3)
+        btnStahp = Button(text="Stop", size_hint_x=1, size_hint_y = 0.3)
         btnStahp.bind(on_press=self.stopSound)
         self.masterlayout.add_widget(btnStahp)
 
